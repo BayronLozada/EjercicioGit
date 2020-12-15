f60b416b7417a788e32d3a88402e07a1fdbd4c86
dam <dam@10.100.100.14>
2020-12-15 12:08:11 +0100

    Se ha creado la rama clasenueva

--- EjercicioGit/src/ejerciciogit/ClaseNueva.java
+++ EjercicioGit/src/ejerciciogit/ClaseNueva.java
@@ -11,6 +11,6 @@ package ejerciciogit;
  */
 public class ClaseNueva {
     public static void main ( String[] args){
-        System.out.println("En la rama. Se crea la clase nueva");
+        System.out.println("En la rama se crea la clase nueva");
     }
 }
